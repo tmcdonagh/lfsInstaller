2e015dc8fef45caa0d92b6a501e30571effcafc2
tom <tmcdonagh12@gmail.com>
2018-06-06 19:23:04 -0500

    Added more steps before compile time

--- second.sh
+++ second.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 echo "exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash" >> ~/.bash_profile
 
+echo "
+set +h
+umask 022
+LFS=/mnt/lfs
+LC_ALL=POSIX
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+PATH=/tools/bin:/bin:/usr/bin
+export LFS LC_ALL LFS_TGT PATH
+" >> ~/.bashrc
+source ~/.bash_profile
+
+cd $LFS/sources
+
+
+
+
+# later
+cores=$(dialog --inputbox "How many cores do you want to use?" 10 25 --output-fd 1)
+
