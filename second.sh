27a99a4983def6efbca1f4128f91063a6883c558
tom <tmcdonagh12@gmail.com>
2018-06-14 13:59:43 -0500

    Added more compile

--- second.sh
+++ second.sh
@@ -69,4 +69,37 @@ do
   touch $file.orig
 done
 
-
+case $(uname -m) in
+  x86_64)
+    sed -e '/m64=/s/lib64/lib' \
+      -i.orig gcc/config/i386/t-linux64
+    ;;
+esac
+mkdir -v build
+cd build
+time {
+../configure \
+  --target=$LFS_TGT \
+  --prefix=/tools \
+  --with-glibc-version=2.11 \
+  --with-sysroot=$LFS \
+  --with-newlib \
+  --without-headers \
+  --with-local-prefix=/tools \
+  --with-native-system-header-dir=/tools/include \
+  --disable-nls \
+  --disable-shared \
+  --disable-multilib \
+  --disable-decimal-float \
+  --disable-threads \
+  --disable-libatomic \
+  --disable-libgomp \
+  --disable-libmpx \
+  --disable-libquadmath \
+  --disable-libssp \
+  --disable-libvtv \
+  --disable-libstdcxx \
+  --enable-languages=c,c++
+make -j $cores
+make install
+}
