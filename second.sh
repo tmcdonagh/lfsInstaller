f1d6f46f74c25d147736c3dd063a35df579d39a8
tom <tmcdonagh12@gmail.com>
2018-06-14 14:50:45 -0500

    Minor Fixes

--- second.sh
+++ second.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 LFS=/mnt/lfs
+sudo ls
 echo "exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash" >> ~/.bash_profile
 
 echo "
@@ -17,7 +18,7 @@ cd $LFS/sources
 cores=$(dialog --inputbox "How many cores do you want to use for compiling?" 10 25 2>&1 > /dev/tty)
 
 # Compiles binutils as it is needed by other packages to compile
-tar -xvf binutils-2.22.tar.bz2
+tar -xf binutils-2.22.tar.bz2
 cd binutils-2.22
 mkdir -v build
 cd build
@@ -29,39 +30,38 @@ time {
   --target=$LFS_TGT \
   --disable-nls \
   --disable-werror &&
-make -j $cores &&
+  make -j $cores &&
 
-case $(uname -m) in
-  x86_64) mkdir -v /tools/lib && ln -sv lib /tools/lib64 ;;
-esac  &&
-make install ;
+  case $(uname -m) in
+    x86_64) mkdir -v /tools/lib && ln -sv lib /tools/lib64 ;;
+  esac  
+  make install ;
 }
 mkdir -v $LFS/sources/finished
 mv $LFS/sources/binutils-2.22.tar.bz2 $LFS/sources/finished
 
 # Compiles GCC and related packages
 cd $LFS/sources
-tar -xvf gcc-4.6.2.tar.bz2 
+tar -xf gcc-4.6.2.tar.bz2 
 cd gcc-4.6.2
 
-tar -xvf ../mpfr-3.1.0.tar.bz2
-mv -v mpfr-3.1.0 mpfr
-tar -xvf ../gmp-5.0.4.tar.xz
-mv -v gmp-5.0.4.tar.xz gmp
-tar -xvf ../mpc-1.1.0.tar.gz
-mv -v mpc-1.1.0 mpc
+tar -xf ../mpfr-3.1.0.tar.bz2
+sudo mv -v mpfr-3.1.0 mpfr
+tar -xf ../gmp-5.0.4.tar.xz
+sudo mv -v gmp-5.0.4.tar.xz gmp
+tar -xf ../mpc-1.1.0.tar.gz
+sudo mv -v mpc-1.1.0 mpc
 
 # Moves tar files to finished directory
-mv $LFS/sources/gcc-4.6.2.tar.bz2 $LFS/sources/finished
-mv $LFS/sources/mpfr-3.1.0.tar.bz2 $LFS/sources/finished
-mv $LFS/sources/gmp-5.0.4.tar.xz $LFS/sources/finished
-mv $LFS/sources/mpc-1.1.0.tar.gz $LFS/sources/finished
+sudo mv $LFS/sources/gcc-4.6.2.tar.bz2 $LFS/sources/finished
+sudo mv $LFS/sources/mpfr-3.1.0.tar.bz2 $LFS/sources/finished
+sudo mv $LFS/sources/gmp-5.0.4.tar.xz $LFS/sources/finished
+sudo mv $LFS/sources/mpc-1.1.0.tar.gz $LFS/sources/finished
 
 for file in gcc/config/{linux,i386/linux{,64}}.h
 do
-  cp -uv $file(,.orig}
-  sed \
-    -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
+  cp -uv $file{,.orig}
+  sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
     -e 's@/usr@/tools@g' $file.orig > $file
   echo '
   #undef STANDARD_STARTFILE_PREFIX_1
@@ -73,13 +73,15 @@ done
 
 case $(uname -m) in
   x86_64)
-    sed -e '/m64=/s/lib64/lib' \
+    sed -e '/m64=/s/lib64/lib/' \
       -i.orig gcc/config/i386/t-linux64
     ;;
 esac
+
+
 mkdir -v build &&
-cd build &&
-../configure \
+  cd build &&
+  ../configure \
   --target=$LFS_TGT \
   --prefix=/tools \
   --with-glibc-version=2.11 \
@@ -101,6 +103,6 @@ cd build &&
   --disable-libvtv \
   --disable-libstdcxx \
   --enable-languages=c,c++ &&
-make -j $cores &&
-make install ;
+  make -j $cores &&
+  make install ;
 }
