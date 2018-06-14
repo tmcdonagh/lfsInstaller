e60d98cc7f3864510001370cc21167421f58ffaf
tom <tmcdonagh12@gmail.com>
2018-06-14 13:40:36 -0500

    Added gcc default dynamic linker

--- second.sh
+++ second.sh
@@ -55,6 +55,18 @@ mv $LFS/sources/mpfr-3.1.0.tar.bz2 $LFS/sources/finished
 mv $LFS/sources/gmp-5.0.4.tar.xz $LFS/sources/finished
 mv $LFS/sources/mpc-1.1.0.tar.gz $LFS/sources/finished
 
-
+for file in gcc/config/{linux,i386/linux{,64}}.h
+do
+  cp -uv $file(,.orig}
+  sed \
+    -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
+    -e 's@/usr@/tools@g' $file.orig > $file
+  echo '
+  #undef STANDARD_STARTFILE_PREFIX_1
+  #undef STANDARD_STARTFILE_PREFIX_2
+  #define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"
+  #define STANDARD_STARTFILE_PREFIX_2 ""' >> $file
+  touch $file.orig
+done
 
 
